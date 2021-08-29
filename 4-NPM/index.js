2fb290c11b1e7337f70bb4f333f5381fc03380e1
David Leal Peláez <davidlealpelaez@gmail.com>
2021-08-29 17:09:03 -0500

    Npm demo

---
+++ 4-NPM/index.js
@@ -0,0 +1,8 @@
+const colors = require('colors');
+
+/* Formas de llamar modulos */
+//Core Node (fs, http, events)
+//Utilizar archivos de modulos
+//Paquetes instalados con NPM
+
+console.log('Hola mundo'.yellow)
